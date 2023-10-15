4e349a7a0f4ae1dc3328b797a3c7cbffed763341
jossjic <jossjic_03@hotmail.com>
2023-10-14 22:52:59 -0600

    global delete

--- back/roundabout.py
+++ back/roundabout.py
@@ -129,7 +129,6 @@ class Block(Agent):
 class TrafficLight(Agent):
     def __init__(self, unique_id, model, pos):
         super().__init__(unique_id, model)
-        global testcount
         self.type = "TrafficLight"
         self.pos = pos
         self.condition = False
